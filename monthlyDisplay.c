af3db33b557a162afd224432ee26945be312a074
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2023-11-21 14:09:22 -0800

    Added base framework, libraries, and working directory code to main file

--- monthlyDisplay.c
+++ monthlyDisplay.c
@@ -6,3 +6,36 @@
  * @version:	1.0
  *
  ****/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include "monthlyDisplay.h"
+#include "iom361_r2.h"
+
+// Defines, Function protoypes, and global variables
+
+int main() {
+	
+	// Local Variables
+
+
+	// Working directory
+	printf("Hi, I made a thing. Caleb Monti (monti@pdx.edu)\n");
+   	errno = 0;
+   	char *buf = getcwd(NULL, 0);    // allocates a buffer large enough to hold the path
+    	if (buf == NULL) {
+       		perror("getcwd");
+        	printf("Could not display the path\n");
+    	}
+    	else {
+        	printf("Current working directory: %s\n", buf);
+        	free(buf);
+    	}
+    	printf("\n\n");
+
+
+	// Main body of code
+}
