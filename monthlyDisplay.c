2e17836f7adfb426a20fbf687154a2625e928623
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2023-11-21 10:18:24 -0800

    Added initial introduction to file, got git set up

--- monthlyDisplay.c
+++ monthlyDisplay.c
@@ -1,1 +1,8 @@
-
+/* monthlyDisplay.c - A program that takes in data from the iom361_r2 module and displays the 
+ * humidity and temperature data over the course of a "month" (not actual data or time)
+ *
+ * @author:	Caleb Monti (monti@pdx.edu)
+ * @date:	21-November-2023
+ * @version:	1.0
+ *
+ ****/
