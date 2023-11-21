e8e0632ffdcad2ec72d5f6a4b9327f85647d68ed
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2023-11-21 10:23:55 -0800

    Created monthlyDisplay.h and added header

---
+++ monthlyDisplay.h
@@ -0,0 +1,8 @@
+/* monthlyDisplay.h - helper file with functions and other useful tools for monthlyDisplay.c
+ *
+ *
+ * @author:	Caleb Monti (monti@pdx.edu)
+ * @date:	21-November-2023
+ * @version:	1.0
+ *
+ ****/
