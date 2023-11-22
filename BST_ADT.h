ee5ead22dec9eb1b9e77ad3f38f92c05f270cf64
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2023-11-22 10:33:00 -0800

    Added header and TODO list for BST_ADT.h to be implemented later

---
+++ BST_ADT.h
@@ -0,0 +1,18 @@
+/* BST_ADT.h - helper function for BST_ADT.c program, using Doxygen-style comments
+ *
+ * @author:	Caleb Monti - monti@pdx.edu
+ * @date:	22-November-2023
+ * @version:	1.0
+ *
+ * @Note:	The functions defined in BST_ADT.c and BST_ADT.h were initially defined by Narasimha Karumanchi, but have been modified to fit the needs of this program.
+ ****/
+
+#include <stdio.h>
+#include <time.h>
+
+// TODO: add function prototype for insertBST
+
+
+// TODO: add function prototype for search
+
+// TODO: add function prototype for preOrder and postOrder
