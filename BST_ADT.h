0b7f62284f6bfcc9807bc47d03fd6cf8a75e2841
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2023-11-23 18:10:47 -0800

    Added function prototypes, variable typedefs, and Doxygen explanations for functions within BST_ADT.c

--- BST_ADT.h
+++ BST_ADT.h
@@ -9,10 +9,81 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-// TODO: add function prototype for insertBST
+
+// define the dataset struct for IOM_361 module containment
+typedef struct dataset {
+
+	time_t timestamp;
+	uint32_t temp;
+	uint32_t humid;
+} Data;
+
+// Define BST node struct
+typedef struct dataTreeNode{
+	Data* data;
+	struct dataTreeNode *left;
+	struct dataTreeNode *right;
+}BST_TreeNode;
+
+// TODO: add a return function within insertBST for error checking.
+/**
+ * insertBST() - inserts a new data node into the sorted BST
+ *
+ * Declares, defines, and places a new node within the Binary Search Tree
+ * with data set by the input. The input must be a struct that contains a
+ * timestamp, as this value is used to place the node within the tree. Does
+ * not return a value
+ *
+ * @param	data_in: a pointer to the incoming data struct, of type Data
+ *
+ *
+ ****/
+void insertBST(Data* data_in);
 
 
-// TODO: add function prototype for search
+
+/**
+ * search() - Searches the BST for a specified timestamp value
+ *
+ * Searches the Binary Search Tree for a timestamp value specified by the input. Once
+ * found, the function then returns the pointer to the found value, or returns a NULL
+ * value if there were no found associations.
+ *
+ * @param	data_in: a type long value timestamp to compare with the BST values
+ *
+ * @return	pointer to the BST node with matching data, or NULL if no match.
+ ****/
+BST_TreeNode* search (long data_in);
 
 // TODO: add function prototype for preOrder and postOrder
+
+/**
+ * preOrder() - Traverses the BST, pre order (root, left, right)
+ *
+ * Traverses the Binary Search Tree in pre-order style, printing the root, then traversing
+ * to the left node and repeating, until the very left node is reached, returning to the
+ * right side, until all nodes have been printed. Printed output will be a sorted list
+ * of all elements in the BST
+ *
+ * @param	root: a pointer to the root node of the BST
+ *
+ ****/
+void preOrder(BST_TreeNode* root);
+
+
+/**
+ * postOrder() - Traverses the BST, post order (left, right, root)
+ *
+ * Traverses the Binary Search Tree in post-order style, traversing to the very left node, 
+ * then to the right node on the same level, then to the root node of that level, and
+ * repeating until all nodes have been printed, with the main root being last. The result
+ * will be a sorted list of all elements within the BST
+ *
+ *
+ * @param	root: a pointer to the root node of the BST
+ *
+ ****/
+void postOrder(BST_TreeNode* root);
+
